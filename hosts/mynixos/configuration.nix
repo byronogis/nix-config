615170788a1036dfe418fcc45503c0a699fc76d8
byronogis <byronogis@outlook.com>
2024-02-11 20:08:34 +0800

    hosts: systemd-boot switch to optional usage

--- hosts/mynixos/configuration.nix
+++ hosts/mynixos/configuration.nix
@@ -8,6 +8,7 @@
     ./hardware-configuration.nix
     
     ../common/global
+    ../common/optional/systmed-boot.nix
   ];
 
   # See https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
--- hosts/mynixos/hardware-configuration.nix
+++ hosts/mynixos/hardware-configuration.nix
@@ -11,13 +11,6 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader = {
-    systemd-boot.enable = true;
-    efi.canTouchEfiVariables = true;
-    timeout = 10;
-  };
-
   # clear the root volume on every boot
   boot.initrd.postDeviceCommands = lib.mkAfter ''
     mkdir /mnt
