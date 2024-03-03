8fdb61cc7311aa29ae150aa20fb8c8012b34ac4b
byronogis <byronogis@outlook.com>
2024-03-03 23:40:56 +0800

    feat(zsh): define a function to set or unset proxy

--- home/common/optional/cli/zsh.nix
+++ home/common/optional/cli/zsh.nix
@@ -12,6 +12,7 @@
       shellAliases = {
         check = "nix flake check --show-trace";
         update = "sudo nixos-rebuild switch --show-trace --flake ";
+        sp = "set_proxy";
       };
       initExtra = ''
         # Search history on up and down keys according input.
@@ -24,6 +25,34 @@
 
         # Fix fnm can not completions postinstall
         eval "$(fnm env --use-on-cd)"
+
+        # Define a function to set or unset proxy
+        set_proxy() {
+          local proxy_url="$1"
+
+          # Check if proxy_url is empty
+          if [ -z "$proxy_url" ]; then
+            # Unset HTTP proxy
+            unset http_proxy
+            unset HTTP_PROXY
+
+            # Unset HTTPS proxy
+            unset https_proxy
+            unset HTTPS_PROXY
+
+            echo "Proxy unset"
+          else
+            # Set HTTP proxy
+            export http_proxy="$proxy_url"
+            export HTTP_PROXY="$proxy_url"
+
+            # Set HTTPS proxy
+            export https_proxy="$proxy_url"
+            export HTTPS_PROXY="$proxy_url"
+
+            echo "Proxy set to: $proxy_url"
+          fi
+        }
       '';
     };
   };
