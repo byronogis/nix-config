7b197e4b3e6136ed03d1c933bda6d305d97a5646
byronogis <byronogis@outlook.com>
2024-03-03 21:25:31 +0800

    fix(zsh): autocomplete not working in nix

--- home/common/optional/cli/zsh.nix
+++ home/common/optional/cli/zsh.nix
@@ -13,6 +13,10 @@
         check = "nix flake check --show-trace";
         update = "sudo nixos-rebuild switch --show-trace --flake ";
       };
+      initExtra = ''
+        bindkey "''${key[Up]}" up-line-or-search
+        bindkey "$key[Down]" down-line-or-search
+      '';
     };
   };
 }
