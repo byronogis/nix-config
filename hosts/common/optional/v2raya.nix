2cbc1dc75a8a32579a7157dbcf919952749ff376
byronogis <byronogis@outlook.com>
2024-03-09 16:19:10 +0800

    chore(host)!: v2raya persist

--- hosts/common/optional/v2raya.nix
+++ hosts/common/optional/v2raya.nix
@@ -1,6 +1,13 @@
-{ pkgs, ... }: {
+{ pkgs, host, ... }: {
   services.v2raya.enable = true;
   networking.firewall.allowedTCPPorts = [
     2017 # default web ui
   ];
+  environment.persistence = {
+    "${host.persistencePath}" = {
+      directories = [
+        "/etc/v2raya"
+      ];
+    };
+  };
 }
