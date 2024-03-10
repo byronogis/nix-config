15a36af52c4846372ca60c24a4b38a7839aaf77f
byronogis <byronogis@outlook.com>
2024-03-10 21:21:49 +0800

    feat(home): add gpg

---
+++ home/common/optional/cli/gpg.nix
@@ -0,0 +1,49 @@
+{ lib
+, pkgs
+, config
+, host
+, user
+, ...
+}:
+let
+  userPublicKeyPath = ../../../${user.username}/gpg.asc;
+  pinentry =
+    if config.gtk.enable then {
+      packages = [ pkgs.pinentry-gnome pkgs.gcr ];
+      name = "gnome3";
+    } else {
+      packages = [ pkgs.pinentry-curses ];
+      name = "curses";
+    };
+in
+
+lib.recursiveUpdate
+{
+  home.packages = pinentry.packages;
+
+  services.gpg-agent = {
+    enable = true;
+    pinentryFlavor = pinentry.name;
+    enableSshSupport = true;
+    enableExtraSocket = true;
+    defaultCacheTtl = 60480000;
+    maxCacheTtl = 60480000;
+  };
+
+  programs.gpg = {
+    enable = true;
+    settings = { };
+    publicKeys = [ ] ++ lib.optionals (builtins.pathExists userPublicKeyPath) [{
+      source = userPublicKeyPath;
+      trust = 5;
+    }];
+  };
+
+}
+  (lib.optionalAttrs host.impermanence {
+    home.persistence."${host.persistencePath}/home/${user.username}" = {
+      directories = [
+        ".gnupg"
+      ];
+    };
+  })
