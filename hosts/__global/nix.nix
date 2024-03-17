987209affd31c367f4d1ac42e1f0cb9485f8b9aa
byronogis <byronogis@outlook.com>
2024-03-17 16:13:10 +0800

    chore(host): add devenv substituters

--- hosts/__global/nix.nix
+++ hosts/__global/nix.nix
@@ -19,9 +19,11 @@
         "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://mirror.sjtu.edu.cn/nix-channels/store"
         "https://nix-community.cachix.org"
+        "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
       trusted-users = [ "root" "@wheel" ];
     };
