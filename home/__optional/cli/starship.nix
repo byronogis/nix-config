84e614c3cc0affd7911f295788d6213b8c3d5430
byronogis <byronogis@outlook.com>
2024-03-18 22:40:49 +0800

    chore(home): adjust shatship settings

--- home/__optional/cli/starship.nix
+++ home/__optional/cli/starship.nix
@@ -3,7 +3,26 @@
     starship = {
       enable = true;
       settings = {
-        # add_newline = false;
+        format = ''$all'';
+        username = {
+          show_always = true;
+          format = "[$user]($style)";
+        };
+        hostname = {
+          ssh_only = false;
+          format = "@[$hostname]($style) ";
+        };
+        shlvl = {
+          format = "[$shlvl]($style)^ ";
+          threshold = 2;
+          repeat = true;
+          disabled = false;
+        };
+        directory = {
+          truncation_length = 5;
+          truncation_symbol = "…/";
+          truncate_to_repo = true;
+        };
       };
     };
   };
