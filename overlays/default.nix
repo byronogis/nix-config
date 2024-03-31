1295bc278e5378a3cfbc8440b67b7c79b1237b59
byronogis <byronogis@outlook.com>
2024-03-31 18:36:29 +0800

    chore: fmt

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,11 +5,12 @@
   # 'inputs.${flake}.legacyPackages.${pkgs.system}'
   flake-inputs = final: _: {
     inputs = builtins.mapAttrs
-      (_: flake: let
-        legacyPackages = ((flake.legacyPackages or {}).${final.system} or {});
-        packages = ((flake.packages or {}).${final.system} or {});
-      in
-        if legacyPackages != {} then legacyPackages else packages
+      (_: flake:
+        let
+          legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
+          packages = ((flake.packages or { }).${final.system} or { });
+        in
+        if legacyPackages != { } then legacyPackages else packages
       )
       inputs;
   };
