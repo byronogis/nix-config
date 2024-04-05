1aa81dd1d2861e3d05f1e6e723b2671d0e2f142f
byronogis <byronogis@outlook.com>
2024-04-05 18:47:36 +0800

    chore(home): update foot setting

--- home/__optional/gui/foot.nix
+++ home/__optional/gui/foot.nix
@@ -3,6 +3,15 @@
     enable = true;
     server.enable = false;
     # https://codeberg.org/dnkl/foot/src/branch/master/foot.ini
-    settings = { };
+    settings = {
+      main = {
+        font = "monospace:size=12";
+      };
+
+      cursor = {
+        style = "beam"; # block underline beam
+        blink = "yes";
+      };
+    };
   };
 }
