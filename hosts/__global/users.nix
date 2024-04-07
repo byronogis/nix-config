291416be91f4a7febd9a10ff944ab726e2c4eeda
byronogis <byronogis@outlook.com>
2024-04-07 20:43:04 +0800

    chore: fmt

--- hosts/__global/users.nix
+++ hosts/__global/users.nix
@@ -4,29 +4,32 @@
 , pkgs
 , ...
 }: {
-  users.users =
-    builtins.mapAttrs
-      (
-        username: user: {
-          extraGroups = [ "wheel" ];
-          # `hashedPassword` > `password` > `hashedPasswordFile` > `initialPassword` > `initialHashedPassword`
-          initialPassword = user.initialPassword;
-          hashedPasswordFile = lib.attrByPath [ "${username}-password" "path" ] null config.sops.secrets;
-          # https://nixos.org/manual/nixos/stable/options#opt-users.users._name_.isNormalUser
-          isNormalUser = true;
-          openssh.authorizedKeys.keys = [
-            (
-              let
-                sshAuthorizedKeysFile = ../../home/${username}/ssh-authorized-keys.pub;
-              in
-              if (builtins.pathExists sshAuthorizedKeysFile) then (builtins.readFile sshAuthorizedKeysFile) else ""
-            )
-          ];
-        }
-      )
-      host.userAttrs;
+  users = {
+    users =
+      builtins.mapAttrs
+        (
+          username: user: {
+            extraGroups = [ "wheel" ];
+            # `hashedPassword` > `password` > `hashedPasswordFile` > `initialPassword` > `initialHashedPassword`
+            initialPassword = user.initialPassword;
+            hashedPasswordFile = lib.attrByPath [ "${username}-password" "path" ] null config.sops.secrets;
+            # https://nixos.org/manual/nixos/stable/options#opt-users.users._name_.isNormalUser
+            isNormalUser = true;
+            openssh.authorizedKeys.keys = [
+              (
+                let
+                  sshAuthorizedKeysFile = ../../home/${username}/ssh-authorized-keys.pub;
+                in
+                if (builtins.pathExists sshAuthorizedKeysFile) then (builtins.readFile sshAuthorizedKeysFile) else ""
+              )
+            ];
+          }
+        )
+        host.userAttrs;
 
-  users.defaultUserShell = pkgs.zsh;
+    defaultUserShell = pkgs.zsh;
+  };
+
   programs.zsh.enable = true;
 
   sops.secrets = lib.mapAttrs'
