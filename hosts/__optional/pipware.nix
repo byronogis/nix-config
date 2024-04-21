5481a0c13e3ac5dd43477192500fc1e0df9d4edf
byronogis <byronogis@outlook.com>
2024-04-21 12:20:22 +0800

    chore(host): update pipeware

--- hosts/__optional/pipware.nix
+++ hosts/__optional/pipware.nix
@@ -3,8 +3,12 @@
     enable = true;
     audio.enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
     jack.enable = true;
     pulse.enable = true;
     wireplumber.enable = true;
   };
+
+  security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
 }
