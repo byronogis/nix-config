7e8e053df31a55205f04a4a0f0ebf9b7aecdc567
byronogis <byronogis@outlook.com>
2024-04-21 16:26:59 +0800

    chore(host): optimize build warn

--- hosts/__global/users.nix
+++ hosts/__global/users.nix
@@ -8,22 +8,26 @@
     users =
       builtins.mapAttrs
         (
-          username: user: {
-            extraGroups = [ "wheel" ];
-            # `hashedPassword` > `password` > `hashedPasswordFile` > `initialPassword` > `initialHashedPassword`
-            initialPassword = user.initialPassword;
-            hashedPasswordFile = lib.attrByPath [ "${username}-password" "path" ] null config.sops.secrets;
-            # https://nixos.org/manual/nixos/stable/options#opt-users.users._name_.isNormalUser
-            isNormalUser = true;
-            openssh.authorizedKeys.keys = [
-              (
-                let
-                  sshAuthorizedKeysFile = ../../home/${username}/ssh-authorized-keys.pub;
-                in
-                if (builtins.pathExists sshAuthorizedKeysFile) then (builtins.readFile sshAuthorizedKeysFile) else ""
-              )
-            ];
-          }
+          username: user:
+            let
+              hashedPasswordFileValue = lib.attrByPath [ "${username}-password" "path" ] null config.sops.secrets;
+            in
+            {
+              extraGroups = [ "wheel" ];
+              # `hashedPassword` > `password` > `hashedPasswordFile` > `initialPassword` > `initialHashedPassword`
+              initialPassword = lib.mkIf (hashedPasswordFileValue == null) user.initialPassword;
+              hashedPasswordFile = hashedPasswordFileValue;
+              # https://nixos.org/manual/nixos/stable/options#opt-users.users._name_.isNormalUser
+              isNormalUser = true;
+              openssh.authorizedKeys.keys = [
+                (
+                  let
+                    sshAuthorizedKeysFile = ../../home/${username}/ssh-authorized-keys.pub;
+                  in
+                  if (builtins.pathExists sshAuthorizedKeysFile) then (builtins.readFile sshAuthorizedKeysFile) else ""
+                )
+              ];
+            }
         )
         host.userAttrs;
 
