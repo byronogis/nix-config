7607a41f29c9862a0a0a67adb6f06a77f46f3669
byronogis <byronogis@outlook.com>
2024-05-03 19:10:21 +0800

    chore: annoying in impermanence

--- flake.nix
+++ flake.nix
@@ -1,10 +1,10 @@
 {
   description = "A nix config based flakes.";
 
-  nixConfig = {
-    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
-  };
+  # nixConfig = {
+  #   extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+  #   extra-substituters = "https://devenv.cachix.org";
+  # };
 
   inputs = {
     # Nixpkgs
