6cbd9ea03ed78bf92576def575cfde968e137b92
byronogis <byronogis@outlook.com>
2024-06-06 10:26:42 +0800

    chore: add comment

--- home/__global/ssh.nix
+++ home/__global/ssh.nix
@@ -6,6 +6,7 @@
   programs.ssh = {
     enable = true;
     matchBlocks = {
+      # See: https://docs.github.com/zh/authentication/troubleshooting-ssh/using-ssh-over-the-https-port
       "github.com" = {
         hostname = "ssh.github.com";
         port = 443;
