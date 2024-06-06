6e70f431ad213bb194bfa9ab0028d450063034b3
byronogis <byronogis@outlook.com>
2024-06-06 08:44:04 +0800

    chore(home): enable ssh and config github.com with https

--- home/__global/ssh.nix
+++ home/__global/ssh.nix
@@ -2,4 +2,15 @@
 , host
 , user
 , ...
-}: { }
+}: {
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github.com" = {
+        hostname = "ssh.github.com";
+        port = 443;
+        user = "git";
+      };
+    };
+  };
+}
