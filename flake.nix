8443d3c75f9586597b919903cbfe6d3a26358e1f
byronogis <byronogis@outlook.com>
2024-06-12 10:35:57 +0800

    chore!: update stable to 24.05
    • Updated input 'nixpkgs-stable':
        'github:nixos/nixpkgs/a3c8d64ba846725f040582b2d3b875466d2115bd' (2024-06-10)
      → 'github:nixos/nixpkgs/47b604b07d1e8146d5398b42d3306fdebd343986' (2024-06-11)

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     # Nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
 
     # Home manager
