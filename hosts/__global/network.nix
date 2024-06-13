ad63528e0953f62bbb0de54feb6515e01afb240a
byronogis <byronogis@outlook.com>
2024-06-13 17:48:56 +0800

    feat(setting): add allowedPortRanges

--- hosts/__global/network.nix
+++ hosts/__global/network.nix
@@ -4,11 +4,14 @@
 }:
 let
   hostAllowedPorts = lib.optionals (host ? "allowedPorts") host.allowedPorts;
+  hostAllowedPortRanges = lib.optionals (host ? "allowedPortRanges") host.allowedPortRanges;
 in
 {
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ ] ++ hostAllowedPorts;
     allowedUDPPorts = [ ] ++ hostAllowedPorts;
+    allowedTCPPortRanges = [ ] ++ hostAllowedPortRanges;
+    allowedUDPPortRanges = [ ] ++ hostAllowedPortRanges;
   };
 }
