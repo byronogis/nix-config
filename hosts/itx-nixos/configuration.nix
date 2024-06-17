3497e40888979f29fed706f01c09530aecfdf661
byronogis <byronogis@outlook.com>
2024-06-17 10:18:17 +0800

    chore(itx-nixos)!: increase max_user_watches

--- hosts/itx-nixos/configuration.nix
+++ hosts/itx-nixos/configuration.nix
@@ -22,6 +22,10 @@
     }
   ];
 
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches" = 524288;
+  };
+
   # See https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "23.11"; # Did you read the comment?
 }
