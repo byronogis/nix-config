7e3a17a62e6e786949ff42c3167f7ec94b01bc35
byronogis <byronogis@outlook.com>
2024-09-12 14:13:17 +0800

    fix(shell): adjust flutter pkg source
    Error resolving plugin [id: 'dev.flutter.flutter-plugin-loader', version: '1.0.0']
    Failed to create directory .. /gradle/.gradle/buildOutputCleanup
    see https://github.com/NixOS/nixpkgs/issues/289936

--- shell/flutter.nix
+++ shell/flutter.nix
@@ -1,9 +1,10 @@
-{
+{ pkgs, ... }: {
   # languages.dart = {
   #   enable = true;
   # };
   android = {
     enable = true;
     flutter.enable = true;
+    flutter.package = pkgs.flutter322;
   };
 }
