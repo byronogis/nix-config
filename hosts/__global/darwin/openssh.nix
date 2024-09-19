07e27e69aebff1263d053b66b7e9f75b4bb33732
byronogis <byronogis@outlook.com>
2024-09-19 20:46:37 +0800

    fix(host): openssh under darwin

--- hosts/__global/darwin/openssh.nix
+++ hosts/__global/darwin/openssh.nix
@@ -1,2 +1,27 @@
 # TODO see https://github.com/LnL7/nix-darwin/issues/627
-{ }
+{ outputs
+, lib
+, config
+, host
+, ...
+}:
+let
+  pubKey = hostname: ./../${hostname}/ssh_host_ed25519_key.pub;
+in
+{
+  programs.ssh = {
+    # Each hosts public key
+    knownHosts =
+      builtins.mapAttrs
+        (hostname: _: {
+          publicKeyFile = pubKey hostname;
+          extraHostNames =
+            lib.optional
+              (
+                hostname == host.hostname
+              )
+              "localhost"; # Alias for localhost if it's the same host
+        })
+        outputs.darwinConfigurations;
+  };
+}
--- hosts/__global/nixos/openssh.nix
+++ hosts/__global/nixos/openssh.nix
@@ -3,7 +3,27 @@
 , config
 , host
 , ...
-}: {
+}:
+let
+  pubKey = hostname: ./../../${hostname}/ssh_host_ed25519_key.pub;
+in
+{
+  programs.ssh = {
+    # Each hosts public key
+    knownHosts =
+      builtins.mapAttrs
+        (hostname: _: {
+          publicKeyFile = pubKey hostname;
+          extraHostNames =
+            lib.optional
+              (
+                hostname == host.hostname
+              )
+              "localhost"; # Alias for localhost if it's the same host
+        })
+        outputs.nixosConfigurations;
+  };
+
   services.openssh = {
     enable = true;
     settings = {
