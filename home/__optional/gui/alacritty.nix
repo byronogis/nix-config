6de954dbc01c0945146aba074817774d339bab0e
byronogis <byronogis@outlook.com>
2024-09-22 16:54:28 +0800

    feat(home): add alacritty

---
+++ home/__optional/gui/alacritty.nix
@@ -0,0 +1,29 @@
+{ config
+, ...
+}: {
+  programs.alacritty = {
+    enable = true;
+    # https://alacritty.org/config-alacritty.html
+    settings = {
+      font = {
+        normal.family = config.fontProfiles.monospace.family;
+        size = 14.0;
+      };
+
+      selection = {
+        save_to_clipboard = true;
+      };
+
+      cursor = {
+        style = {
+          shape = "Beam";
+          blinking = "On";
+        };
+      };
+
+      window = {
+        startup_mode = "Maximized";
+      };
+    };
+  };
+}
