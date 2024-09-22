50cd801d8411235de2a5d215c106f60cf4286a69
byronogis <byronogis@outlook.com>
2024-09-22 11:12:21 +0800

    feat(home/optional): add zellij and update kitty

--- home/__optional/gui/kitty.nix
+++ home/__optional/gui/kitty.nix
@@ -1,5 +1,18 @@
 { ... }: {
   programs.kitty = {
     enable = true;
+
+    font = {
+      name = "Hack Nerd Font";
+      size = 14;
+    };
+
+    settings = {
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
+      macos_option_as_alt = true;
+    };
+
+    darwinLaunchOptions = ["--start-as=maximized"];
   };
 }
