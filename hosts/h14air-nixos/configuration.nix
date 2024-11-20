8772a2da4b311ec3f45cf5f0999ee9a1d631d8b6
byronogis <byronogis@outlook.com>
2024-11-20 14:33:15 +0800

    Revert "chore(hosts/h14air-nixos)!: move to wsl"
    This reverts commit 65a4af25fa4c5f7354228d8be09091f413c46a72.

--- hosts/h14air-nixos/configuration.nix
+++ hosts/h14air-nixos/configuration.nix
@@ -5,13 +5,28 @@
 , ...
 }: {
   imports = [
-    ../__optional/wsl.nix
+    ./hardware-configuration.nix
+    ./disko-config.nix
+
+    ../__optional/systemd-boot.nix
     ../__optional/nix-ld.nix
-    # ../__optional/v2raya.nix
-    # ../__optional/zerotierone.nix
+    ../__optional/v2raya.nix
     ../__optional/podman.nix
+    # ../__optional/zerotierone.nix
   ];
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  networking.firewall.enable = lib.mkForce false;
+
+  swapDevices = [
+    {
+      device = "/swap/swapfile";
+      size = 16 * 1024; # in megabytes
+    }
+  ];
+
+  boot.kernel.sysctl = {
+    "fs.inotify.max_user_watches" = 524288;
+  };
+
   system.stateVersion = "24.05";
 }
