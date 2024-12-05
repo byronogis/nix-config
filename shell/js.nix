90df232cc42327b9412954da521c57ac3009210d
byronogis <byronogis@outlook.com>
2024-12-05 14:35:17 +0800

    chore(shell): update js

--- shell/js.nix
+++ shell/js.nix
@@ -1,7 +1,13 @@
-{
-  languages.javascript = {
-    enable = true;
-    bun.enable = true;
-    corepack.enable = true;
-  };
+{ pkgs, lib, ... }: {
+  # languages.javascript = {
+  #   enable = true;
+  #   bun.enable = true;
+  #   corepack.enable = true;
+  # };
+
+  packages = with pkgs; [
+    # nodejs-slim # managed by fnm
+    deno
+    bun
+  ];
 }
