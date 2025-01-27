04878877320dba02f867ba74cafd937c502ef2c3
byronogis <byronogis@outlook.com>
2025-01-27 10:55:07 +0800

    chore: fmt

--- home/__optional/cli/helix.nix
+++ home/__optional/cli/helix.nix
@@ -20,7 +20,7 @@
       };
       keys.normal = {
         # [auto wrap](https://github.com/helix-editor/helix/issues/136#issuecomment-1925898113)
-        space.W = [":toggle soft-wrap.enable" ":redraw"];
+        space.W = [ ":toggle soft-wrap.enable" ":redraw" ];
       };
     };
   };
