a17a4b815dfdd73648b982a68a8b425b240cc025
byronogis <byronogis@outlook.com>
2025-06-02 08:55:33 +0800

    chore(yazi)!: `[manager]` has been deprecated in favor of the new `[mgr]`

--- home/__optional/cli/yazi.nix
+++ home/__optional/cli/yazi.nix
@@ -5,7 +5,7 @@
     enable = true;
     enableZshIntegration = true;
     settings = {
-      manager = {
+      mgr = {
         sort_by = "natural";
         sort_sensitive = false;
         sort_dir_first = true;
