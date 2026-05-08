6c9e17d9ba529afc470e131c6927807ca8a0a093
byronogis <byronogis@outlook.com>
2026-05-08 14:28:04 +0800

    feat(flake): update impermanence inputs to follow nixpkgs and home-manager

--- flake.nix
+++ flake.nix
@@ -31,7 +31,11 @@
     };
 
     # Persistence
-    impermanence.url = "github:nix-community/impermanence";
+    impermanence = {
+      url = "github:nix-community/impermanence";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.home-manager.follows = "home-manager";
+    };
 
     # Disk Management
     disko = {
