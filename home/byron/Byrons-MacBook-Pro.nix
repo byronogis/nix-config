7f669f89383801ce32899b3129c72bacc66f95a5
byronogis <byronogis@outlook.com>
2026-07-04 20:09:04 +0800

    chore: add 'just' to home packages and enable 'ai' import

--- home/byron/Byrons-MacBook-Pro.nix
+++ home/byron/Byrons-MacBook-Pro.nix
@@ -11,7 +11,7 @@
     ../__optional/desktop/__base/font.nix
     ../__optional/tmux/adapters/kaku.nix
     # ../__optional/gui/alacritty.nix
-    # ../__optional/ai.nix
+    ../__optional/ai.nix
     # ../__optional/gui/kitty.nix
     # ../__optional/gui/zellij/zellij.nix
   ];
